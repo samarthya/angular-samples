4c9193118152ff1802ae61a26ddeeecb875470ff
Saurabh Sharma <saurabh.sharma@pb.com>
2016-03-24 13:21:47 +0530

    Added scope timeout and messages

--- examples/app.js
+++ examples/app.js
@@ -1,7 +1,27 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages']);
 
-myApp.controller('myController', ['$scope', '$log', function ($scope, $log) {
+myApp.controller('myController', ['$scope', '$log', '$timeout', function ($scope, $log, $timeout) {
     'use strict';
+    
     $scope.sampleText = " This is a sample text information using Angular way of life";
+    
     $log.info(" sample log information");
+    
+    /**
+     * Angular service of timeout.
+     * Angular's wrapper for window.setTimeout
+     */
+    $timeout(function () { 
+        $log.warn(" Timeout called!");
+        $scope.sampleText = "Timeout text appears";
+    }, 3000);
+    
+    setTimeout(function () {
+        console.warn("Indepedent timer outside Angular context called ");
+        $scope.sampleText = "Outside angular context - so wont appear!";
+        $scope.$apply(function () {
+            $scope.sampleText = "Instigating a digest cycle.";
+        });
+    }, 4000);
+    
 }]);
