4c2f65aad53df0d15c068dbbcef56adad311b052
Saurabh Sharma <saurabh.sharma@pb.com>
2016-03-24 12:08:44 +0530

    Added log statement

--- examples/app.js
+++ examples/app.js
@@ -1,6 +1,7 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('myController', ['$scope', function ($scope) {
+myApp.controller('myController', ['$scope', '$log', function ($scope, $log) {
     'use strict';
     $scope.sampleText = " This is a sample text information using Angular way of life";
+    $log.info(" sample log information");
 }]);
