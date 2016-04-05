d6dfa4110f08b5d5cba21697a14be09196ca4100
saurabh777@gmail.com <saurabh.sharma@pb.com>
2016-04-05 19:14:19 +0530

    Path changes to bower

--- examples/example2/app.js
+++ examples/example2/app.js
@@ -1,27 +1,12 @@
 var myApp = angular.module('myApp', ['ngMessages']);
 
-myApp.controller('myController', ['$scope', '$log', '$timeout', function ($scope, $log, $timeout) {
+myApp.controller('myController', ['$scope', '$log', '$timeout', '$location', function ($scope, $log, $timeout, $location) {
     'use strict';
     
     $scope.sampleText = " This is a sample text information using Angular way of life";
     
     $log.info(" sample log information");
+    $log.info($location.path());
     
-    /**
-     * Angular service of timeout.
-     * Angular's wrapper for window.setTimeout
-     */
-    $timeout(function () { 
-        $log.warn(" Timeout called!");
-        $scope.sampleText = "Timeout text appears";
-    }, 3000);
-    
-    setTimeout(function () {
-        console.warn("Indepedent timer outside Angular context called ");
-        $scope.sampleText = "Outside angular context - so wont appear!";
-        $scope.$apply(function () {
-            $scope.sampleText = "Instigating a digest cycle.";
-        });
-    }, 4000);
     
 }]);
